4b210e383ebcf231bc277f4605419c01d92073c4
pablo unzueta <punzu001@ucr.edu>
2024-08-21 12:58:47 -0700

    try again

--- qcio/models/base_models.py
+++ qcio/models/base_models.py
@@ -325,8 +325,8 @@ class CalcType(str, Enum):
     optimization = "optimization"
     transition_state = "transition_state"
     conformer_search = "conformer_search"
-    meci_optimization = "conical"
-
+    conical = "conical"
+    
     def __repr__(self) -> str:
         """Custom repr for CalcType"""
         return f"'{self.name}'"  # pragma: no cover
